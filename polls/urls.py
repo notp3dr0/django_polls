dfd7e413f77f839e0869cd80f028e79b5da28618
mitcy saraiva <saraivamitcy@gmail.com>
2023-06-20 11:02:54 -0300

    Inclui página com as últimas perguntas.

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao')
    
 ]
+
+
--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from polls.models import Question, Choice
+
+def ultimas_perguntas(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/perguntas.html', context)
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse('Olá... seja bem vindo a enquete')
+
+def sobre(request):
+    return HttpResponse('Este é um app de enquete!')
+
+def exibe_questao(request, question_id):
+    questao = Question.objects.get(id=question_id)
 
