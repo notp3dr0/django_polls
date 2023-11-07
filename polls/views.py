531914139d652ca0cd882a719c837d9b877fd666
notp3dr0 <guto.siqueiralino@gmail.com>
2023-11-07 11:04:06 -0300

    Paginação simples com ListView.

--- polls/views.py
+++ polls/views.py
@@ -75,3 +75,10 @@ class QuestionListView(ListView):
 
 class SobreTemplateView(TemplateView):
     template_name = 'polls/sobre.html'
+
+class QuestionListView(ListView):
+    model = Question
+    template_name = 'polls/question_list.html'
+    context_object_name = 'questions'
+    paginate_by = 5 # quantidade de itens por página
+    ordering = ['-pub_date'] # ordenar pela data de publicação de forma inversão    
